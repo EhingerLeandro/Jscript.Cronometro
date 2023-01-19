c424f278bcdb9a0d76c062f4fc0f0f26cbfee6e1
EhingerLeandro <ehinger.leandro@hotmail.com>
2023-01-19 15:30:08 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const buttonStart= document.querySelector('#start');
+const buttonStop= document.querySelector("#stop");
+const buttonClear= document.querySelector("#clear"); 
+
+let time=0;
+
+//Crear función contador (Start)
+const Start =()=>{
+
+}
+
+//Crear función parar (Stop)
+const Stop = () =>{
+
+}
+
+//Crear función limpiar (Clear)
+const clear = ()=>{
+
+}
+
+//Crear función render
+const render = ()=>{
+
+}
